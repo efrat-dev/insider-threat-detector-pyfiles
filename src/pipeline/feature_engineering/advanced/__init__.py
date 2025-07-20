b75bd8bc27bcf6fb70e85dd0b63dbc959c1c8662
efchbd1013 <efchbd1013@gmail.com>
2025-07-21 00:04:42 +0300

    Remove reference to unexist file

--- src/pipeline/feature_engineering/advanced/__init__.py
+++ src/pipeline/feature_engineering/advanced/__init__.py
@@ -4,7 +4,6 @@ from .temporal_features import TemporalFeatureEngineer
 from .risk_profile_features import RiskProfileFeatureEngineer
 from .anomaly_features import AnomalyFeatureEngineer
 from .advanced_interaction_features import AdvancedInteractionFeatureEngineer
-from .feature_analysis import FeatureAnalyzer
 
 __all__ = [
     'BehavioralFeatureEngineer',
@@ -12,5 +11,4 @@ __all__ = [
     'RiskProfileFeatureEngineer',
     'AnomalyFeatureEngineer',
     'AdvancedInteractionFeatureEngineer',
-    'FeatureAnalyzer'
 ]
